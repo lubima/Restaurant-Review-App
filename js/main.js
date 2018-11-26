55915b021c8234fcd8dfe55d9d99a8cc792d45bf
Lyubima Toshkova <lyubimatoshkova18@gmail.com>
2018-11-26 11:36:20 -0600

    feat:MapBox API key added

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoibHViaW1hIiwiYSI6ImNqb3lsYjM1ZTJnc3Eza21qOXozenRma28ifQ.leViOVVL3r9VtsxiYqYUfA',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
