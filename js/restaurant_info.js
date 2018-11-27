8f544ea1d5bb90649883ad6a64407d8df12de5bd
Lyubima Toshkova <lyubimatoshkova18@gmail.com>
2018-11-27 11:40:21 -0600

    feat:Alternative text added to all the images

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -89,6 +89,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = restaurant.altText;
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
